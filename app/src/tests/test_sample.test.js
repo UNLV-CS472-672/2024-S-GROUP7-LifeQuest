7b43e51f574c24ec8b7a136afb156edf0b3bdb67
dusek2 <dusejovsky@gmail.com>
2024-03-12 16:11:33 -0700

    Fixing workflow to work with node.js

---
+++ app/src/tests/test_sample.test.js
@@ -0,0 +1,4 @@
+// sample test to be start our coverage testing
+test('synchronous passing test', () => {
+  expect(1).toBe(1);
+});
--- tests/test_nothing.py
+++
@@ -1,9 +0,0 @@
-"""
-Test Cases for Testing tests
-
-"""
-
-from unittest import TestCase
-
-class NothingTest(TestCase):
-    """Nothing tests"""
