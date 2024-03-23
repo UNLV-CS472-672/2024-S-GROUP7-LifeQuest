e8f239a9f45c3dcb4e1354c87960c61c184e3ffd
Alexander S <alexschupp365@gmail.com>
2024-03-22 17:54:59 -0700

    Created Level Select
    Created a level select scene with popout menus for confirmation. Also added a scripts folder to better organize scripts.

--- Game/LifeQuest/main.gd
+++ Game/LifeQuest/main.gd
@@ -7,7 +7,7 @@ extends Node2D
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://game_scene.tscn")
+	get_tree().change_scene_to_file("res://map_scene.tscn")
 
 
 func _on_quit_pressed():
