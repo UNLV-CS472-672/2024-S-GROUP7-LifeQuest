cfabb0d31e95e78900780c945c092f8d7e993490
FernFinder <fernandolrojas00@gmail.com>
2024-03-26 21:05:51 -0700

    Updated login form and test unit

--- app/src/pages/login/Login.js
+++ app/src/pages/login/Login.js
@@ -51,13 +51,13 @@ const Login = () => {
     return (
     <div className="login_container">
         <form onSubmit={sendSubmit} className="login_form">
-            <label for="email">Email</label>
+            <label htmlFor ="email">Email</label>
             <input value={email} type="email" onChange={(foo) => setEmail(foo.target.value)} placeholder="Email" name="Email" id="Email" />
-            <label for="password">Password</label>
+            <label htmlFor ="password">Password</label>
             <input value={password} type="password" onChange={(foo) => setPassword(foo.target.value)} placeholder="********" name="password" id="password" />
-            <button class="login_button" type="submit">Log In</button>
+            <button className="login_button" type="submit">Log In</button>
         </form>
-        <button class="register_button">Don't have a LifeQuest account? <br/> Register here.</button>
+        <button className="register_button">Don't have a LifeQuest account? <br/> Register here.</button>
     </div>
   );
 };
--- app/src/tests/LoginPage.test.js
+++ app/src/tests/LoginPage.test.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import LoginPage from '../pages/login/Login.js';
+import {
+    Routes,
+    Route,
+    BrowserRouter
+  } from "react-router-dom";
+import '@testing-library/jest-dom';
 
 describe('LoginPage', () => {
     it('renders without crashing', () => {
-        const {getByTestId, getByLabelText} = render(<LoginPage />);
+        const {getByText, getByLabelText} = render(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+        );
+        
+
       
-        const emailLabel = getByText(/email:/i)
-        const passwordLabel = getByText(/password:/i)
+        const emailLabel = getByText(/Email/i)
+        const passwordLabel = getByText(/Password/i)
 
         expect(emailLabel).toBeInDocument()
         expect(passwordLabel).toBeInDocument()
 
-        const emailInput = getByLabelText(/email:/i);
+        const emailInput = getByLabelText(/Email/i);
         expect(emailInput).toHaveAttribute('type', 'email');
 
-        const passwordInput = getByLabelText(/password:/i);
+        const passwordInput = getByLabelText(/Password/i);
         expect(passwordInput).toHaveAttribute('type', 'password');
 
     });
