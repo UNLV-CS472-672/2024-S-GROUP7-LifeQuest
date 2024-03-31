5b16b8d325f7f149ae8fa2796d4e3cb0cce44a3d
dusek2 <dusejovsky@gmail.com>
2024-03-31 13:45:21 -0700

    Adding coverage to app.js

--- app/src/components/TopPanel.js
+++ app/src/components/TopPanel.js
@@ -3,13 +3,11 @@ import styles from "./TopPanel.module.css";
 
 const TopPanel = () => {
   return (
-    <a href="/">
       <div className={styles.frame}>
       <div className={styles.topPanel} data-testid="top-panel">
             <BackButton backbuttonposition="relative" />
         </div>
       </div>
-    </a>
   );
 };
 
--- app/src/tests/AppJs.test.js
+++ app/src/tests/AppJs.test.js
@@ -26,5 +26,41 @@ describe("App Routing", () => {
     expect(screen.getByText("Email")).toBeInTheDocument();
   });
 
+  test("renders Home component when navigating to '/home'", () => {
+    render(
+      <MemoryRouter initialEntries={["/home"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText("LifeQuest")).toBeInTheDocument();
+  });
+
+  test("renders Settings component when navigating to '/settings'", () => {
+    render(
+      <MemoryRouter initialEntries={["/settings"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText("Settings Page")).toBeInTheDocument();
+  });
+
+  test("renders Quests component when navigating to '/quests'", () => {
+    render(
+      <MemoryRouter initialEntries={["/quests"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText("Quests Page")).toBeInTheDocument();
+  });
+
+  test("renders Quiz component when navigating to '/quiz'", () => {
+    render(
+      <MemoryRouter initialEntries={["/quiz"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText("Strongly Agree")).toBeInTheDocument();
+  });
+
 });
 
