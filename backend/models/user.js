fb70b593d13e5d3d232e234d8e6082be0a5b0ede
dusek2 <dusejovsky@gmail.com>
2024-04-04 14:17:20 -0700

    Improving user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,21 +1,22 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   // user info
-  username: String,
-  password: String,
-  email: String,
-  completedQuiz: Boolean,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  completedQuiz: { type: Boolean, default: false },
   // life stats
   stats: {
-    MeStat: Number,
-    WorkStat: Number,
-    LoveStat: Number,
+    MeStat: { type: Number, default: 0 },
+    WorkStat: { type: Number, default: 0 },
+    LoveStat: { type: Number, default: 0 },
   },
   // user's quests
   quests: [{ type: Schema.Types.ObjectId, ref: "Quest" }],
   // user's settings
-  settings: Object,
+  settings: { type: Schema.Types.ObjectId, ref: "Settings" },
 });
 
 module.exports = mongoose.model("User", userSchema);
