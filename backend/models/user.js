e78cfcbf8cf499eabab7b0b826bc9a7ceb3b0e0c
dusek2 <dusejovsky@gmail.com>
2024-04-04 13:59:37 -0700

    adding schemas for quests and settings

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    password: String
-})
+const userSchema = new Schema({
+  // user info
+  username: String,
+  password: String,
+  email: String,
+  completedQuiz: Boolean,
+  // life stats
+  stats: {
+    MeStat: Number,
+    WorkStat: Number,
+    LoveStat: Number,
+  },
+  // user's quests
+  quests: [{ type: Schema.Types.ObjectId, ref: "Quest" }],
+  // user's settings
+  settings: Object,
+});
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
