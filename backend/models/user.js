ed62144c30c54d33cd9ac8186b23aa6f14d967c0
Kamil Dusejovsky <74691395+dusek2@users.noreply.github.com>
2024-04-04 18:46:53 -0700

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,6 +5,10 @@ const userSchema = new Schema({
   // user info
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  name: {
+    first: String,
+    last: String,
+  },
   completedQuiz: { type: Boolean, default: false },
   // life stats
   stats: {
