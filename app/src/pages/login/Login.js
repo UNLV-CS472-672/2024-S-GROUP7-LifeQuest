6200a816784a5baf3f9bf6bf66c38ba506402caa
FernFinder <130785206+FernFinder@users.noreply.github.com>
2024-04-14 15:20:05 -0700

    Fixed testing issues.

--- app/src/pages/login/Login.js
+++ app/src/pages/login/Login.js
@@ -41,8 +41,8 @@ const Login = () => {
             <h1>LifeQuest</h1>
             {/* The login form which is tied to function sendSubmit() */}
             <form onSubmit={sendSubmit} className="login_form">
-                <input class="oval" value={email} type="email" onChange={(foo) => setEmail(foo.target.value)} placeholder="Email" name="Email" id="Email" />
-                <input class="oval" value={password} type="password" onChange={(foo) => setPassword(foo.target.value)} placeholder="Password" name="Password" id="Password" />
+                <input className="oval" value={email} type="email" onChange={(foo) => setEmail(foo.target.value)} placeholder="Email" name="Email" id="Email" />
+                <input className="oval" value={password} type="password" onChange={(foo) => setPassword(foo.target.value)} placeholder="Password" name="Password" id="Password" />
                 {/* The forgot password link */}
                 <a className="forgot_password" href="url">Forgot password?</a>
                 {/* The login button */}
