c681447b1565142f9462232ab1c77d2f9d52c70f
FernFinder <130785206+FernFinder@users.noreply.github.com>
2024-04-24 02:10:36 -0700

    Comments for test.

--- backend/tests/users.notfound.test.js
+++ backend/tests/users.notfound.test.js
@@ -1,13 +1,8 @@
 const request = require("supertest");
 const sinon = require('sinon');
 const middleware = require('../middleware/auth.js');
-const User = require('../models/user');
 const mongoose = require('mongoose');
 var app;
-
-let userEmail = "john" + Math.random() + "@doesnotexist.com";
-let userPassword = "ABC" + Math.random();
-
 
 beforeAll(async () => {
   //Replace the middleware function with a fake function that passes the request through.
@@ -29,11 +24,11 @@ afterAll(async () => {
 // test user stat change
 describe("POST /users/changestats", () => {  
   it("it should return 404 for no user found", async () => {
-      // post request to create a new user
-      const response = await request(app).post("/users/changestats");
-      // check if correct user in reponse
-      expect(response.statusCode).toBe(404);
-      expect(response.body.message).toBe('User not found');
+    // request stat update
+    const response = await request(app).post("/users/changestats");
+    // check for error 404, user not found
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe('User not found');
   });
 });
 
@@ -41,7 +36,7 @@ describe("GET /users/me", () => {
   it("it should get a user's stats", async () => {
     // request stats
     const response = await request(app).get("/users/me");
-    // check for success
+    // check for error 404, user not found
     expect(response.statusCode).toBe(404);
     expect(response.body.message).toBe('User not found');
   });
